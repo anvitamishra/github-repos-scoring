5a89eb116d059dd638a97ecddb4d86a61e59ceb0
Anvita Mishra <anvitamishra309@gmail.com>
2024-09-26 15:51:16 +0200

    Add popularity score calculation and a sample test

--- app.js
+++ app.js
@@ -1,26 +1,161 @@
-import { Octokit } from '@octokit/core'
+import { Octokit } from '@octokit/rest'
+import { throttling } from '@octokit/plugin-throttling'
+
+import pLimit from 'p-limit'
 
 const USER_TOKEN = process.env.USER_TOKEN
 const LANGUAGE = process.env.LANGUAGE
 const DATE = process.env.DATE
 
+let MAX_STARS = Number.MIN_SAFE_INTEGER
+let MAX_FORKS = Number.MIN_SAFE_INTEGER
+let MAX_DAYS_SINCE_UPDATE = 0 // No need for minimum here
+
+const limit = pLimit(10)
+
+const OctokitWithPlugin = Octokit.plugin(throttling)
+
 // Octokit.js
 // https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-  auth: USER_TOKEN
+const octokit = new OctokitWithPlugin({
+  auth: USER_TOKEN,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  },
+  throttle: {
+    onRateLimit: (retryAfter, options) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`,
+      )
+
+      // Retry twice after hitting a rate limit error, then give up
+      if (options.request.retryCount <= 2) {
+        console.log(`Retrying after ${retryAfter} seconds!`)
+        return true
+      }
+    },
+    onSecondaryRateLimit: (retryAfter, options, octokit) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `Secondary quota detected for request ${options.method} ${options.url}`,
+      )
+    },
+  },
 })
 
-const main = async() => {
-  const queryString = encodeURIComponent(`input-quantity language:${LANGUAGE} created:=>${DATE}`)
+/**
+ * Gets all the pages of the repositories from the GitHub API based on
+ *   a language and date query.
+ */
+const getRepositories = async() => {
+  const queryString = `language:${LANGUAGE} created:>=${DATE}`
 
-  // https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-repositories
-  const response = await octokit.request(`GET /search/repositories?q=${queryString}&per_page=10`, {
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
+  let allRepositories = []
+
+  for await (const response of octokit.paginate.iterator(octokit.rest.search.repos,
+    {
+      q: queryString,
+      per_page: 100
     }
-  })
+  )) {
+    const { data } = response
+    const repositories = data.map(item => ({
+      id: item.id,
+      name: item.name,
+      language: item.language,
+      created_at: item.created_at,
+      stargazers_count: item.stargazers_count,
+      forks: item.forks,
+      updated_at: item.updated_at,
+    }))
 
-  const { data } = response
+    allRepositories = allRepositories.concat(repositories)
+  }
+  return allRepositories
+}
 
-  console.log(data)
+/**
+ * Updates the max values for stars, forks, and days since last update
+ *   across all repositories.
+ * [ ChatGPT suggested that I use these to normalize the data. ]
+ */
+const getMaxValues = async (repositories) => {
+
+  for (const repo of repositories) {
+    const { stargazers_count: stars, forks } = repo
+
+    const daysSinceUpdate = Math.floor((Date.now() - new Date(repo.updated_at).getTime()) / (1000 * 60 * 60 * 24))
+
+    MAX_STARS = Math.max(MAX_STARS, stars)
+    MAX_FORKS = Math.max(MAX_FORKS, forks)
+    MAX_DAYS_SINCE_UPDATE = Math.max(MAX_DAYS_SINCE_UPDATE, daysSinceUpdate)
+  }
+
+  return { maxStars: MAX_STARS, maxForks: MAX_FORKS, maxDaysSinceUpdate: MAX_DAYS_SINCE_UPDATE }
+}
+
+/**
+ * Computes the popularity score for a repository based on the max values
+ *   for stars, forks, and days since last update.
+ * [ Asked ChatGPT to give me a method for score calculation and refactored that ]
+ */
+const scoringAlgorithm = async (repository, { maxStars, maxForks, maxDaysSinceUpdate }) => {
+  const today = Date.now()
+  const factors = ['stargazers_count', 'forks', 'updated_at']
+
+  const popularityScore = factors.reduce((score, factor) => {
+    const weight = 1 / factors.length
+    let value = repository[factor]
+
+    let normalized
+    switch (factor) {
+      case 'updated_at':
+        const daysSinceLastUpdate = Math.floor((today - new Date(value).getTime()) / (1000 * 60 * 60 * 24))
+        normalized = 1 - (daysSinceLastUpdate / maxDaysSinceUpdate)
+        break
+      case 'forks':
+        normalized = value / Math.max(maxForks, 1)
+        break
+      case 'stargazers_count':
+        normalized = value / Math.max(maxStars, 1)
+        break
+    }
+
+    score += normalized * weight
+
+    return score
+  }, 0)
+
+  const result = {
+    name: repository.name,
+    popularityScore: popularityScore.toFixed(3)
+  }
+
+  return result
+}
+
+/**
+ * Outputs the popularity scores for all the repositories.
+ */
+const main = async () => {
+  const repositories = await getRepositories()
+  const maxValues = await getMaxValues(repositories)
+
+  const promises = repositories.map(repo => limit(() => scoringAlgorithm(repo, maxValues)))
+
+  for (const promise of promises) {
+    const popularityScore = await promise
+    console.log(popularityScore)
+  }
+}
+
+const startTime = performance.now()
+await main()
+const endTime = performance.now()
+
+console.log('Total time taken:', (endTime - startTime) / 1000, 'seconds')
+
+export {
+  getMaxValues,
+  scoringAlgorithm
 }
--- app.test.js
+++ app.test.js
@@ -1,1 +1,58 @@
 import test from 'ava'
+
+import { scoringAlgorithm, getMaxValues } from './app.js'
+
+const repositories = [
+  {
+    id: 787368172,
+    name: 'garageflipper',
+    language: 'Dart',
+    created_at: '2024-04-16T11:48:36Z',
+    stargazers_count: 5,
+    forks: 7,
+    updated_at: '2024-04-16T11:56:50Z'
+  },
+  {
+    id: 811166938,
+    name: 'QR-Inventory-Tracke',
+    language: 'Dart',
+    created_at: '2024-06-06T04:24:27Z',
+    stargazers_count: 1000,
+    forks: 99,
+    updated_at: '2024-06-06T04:31:03Z'
+  },
+  {
+    id: 856009755,
+    name: 'fzbill',
+    language: 'Dart',
+    created_at: '2024-09-11T20:37:13Z',
+    stargazers_count: 10,
+    forks: 18,
+    updated_at: '2024-09-11T20:37:27Z'
+  },
+  {
+    id: 641518970,
+    name: 'assessment-prob1',
+    language: 'Dart',
+    created_at: '2023-05-16T16:30:50Z',
+    stargazers_count: 0,
+    forks: 0,
+    updated_at: '2023-05-16T16:43:13Z'
+  },
+  {
+    id: 652503391,
+    name: 'csv_order_analytics',
+    language: 'Dart',
+    created_at: '2023-06-12T07:53:40Z',
+    stargazers_count: 1,
+    forks: 1,
+    updated_at: '2023-07-12T12:57:22Z'
+  }
+]
+
+test('Should calculate a popularity score', async t => {
+    const maxValues = await getMaxValues(repositories)
+    const results = await Promise.all(repositories.map(repo => scoringAlgorithm(repo, maxValues)))
+
+    t.true(results.every(repo => repo.popularityScore >= 0 && repo.popularityScore <= 1), 'Final score should be between 0 and 1')
+  })
